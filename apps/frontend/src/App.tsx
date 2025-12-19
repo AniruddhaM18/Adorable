c41939f2a071340fd59a04ae2d7e0caaabb6fc3e
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-12-19 23:09:28 +0530

    basic fe

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import CodeEditor from './components/CodeEditor'
+
+function App() {
+  return (
+    <>
+    <div style={{backgroundColor: "red"}}>
+     <CodeEditor />
+     </div>
+    </>
+  )
+}
+
+export default App
