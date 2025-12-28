19d2856c6bedf3e8822568f54b8edfb2db9a0b1c
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-12-28 15:30:57 +0530

    changes

--- apps/backend/src/controllers/projectController.ts
+++ apps/backend/src/controllers/projectController.ts
@@ -1,5 +1,6 @@
+import { prisma } from "@repo/database";
 import { Request, Response } from "express";
 
 export async function createProject(req: Request, res: Response) {
-
+    
 }
--- apps/backend/src/routes/projectRouter.ts
+++ apps/backend/src/routes/projectRouter.ts
@@ -3,7 +3,6 @@ import { createProject } from "../controllers/projectController.js";
 
 const projectRouter: Router = Router();
 
-
 projectRouter.post("/create", createProject);
 
 export default projectRouter;
