f8bef04e20bf1ea38284e79f47c7f13c62848b2c
Aniruddha <aniruddhamaradwar9@gmail.com>
2026-01-03 12:55:49 +0530

    auth-pages

--- apps/web/components/signup-form.tsx
+++ apps/web/components/signup-form.tsx
@@ -1,4 +1,3 @@
-import { GalleryVerticalEnd } from "lucide-react"
 import Image from "next/image";
 import logo from "../public/adlogo.png"
 import { cn } from "@/lib/utils"
@@ -32,41 +31,41 @@ export function SignupForm({
                 height={200}
                 width={200}
                  />
-
-
               </div>
               <span className="sr-only">Adorable</span>
             </a>
-            <h1 className="text-xl font-bold">Welcome to Adorable</h1>
-            <FieldDescription>
+            <h1 className="text-xl font-bold text-neutral-100">Welcome to Adorable</h1>
+            <FieldDescription className="text-neutral-300">
               Already have an account? <a href="#">Sign in</a>
             </FieldDescription>
           </div>
           <Field>
-            <FieldLabel htmlFor="email">Email</FieldLabel>
+            <FieldLabel className="text-neutral-200" htmlFor="email">Email</FieldLabel>
             <Input
+              className="text-neutral-300 border-neutral-500"
               id="email"
               type="email"
-              placeholder="m@example.com"
+              placeholder="yourname@mail.com"
               required
             />
           </Field>
           <Field>
-          <Field>
+          <Field className="text-neutral-200">
             <FieldLabel htmlFor="password">Password</FieldLabel>
-            <Input
+            <Input className="border-neutral-500"
               id="password"
               type="password"
               required
             />
           </Field>
-          <Field></Field>
-            <Button type="submit">Create Account</Button>
+          <Button className="bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-400 text-neutral-800 transition-all duration-200 ease-in-out active:scale-95" type="submit">Create Account</Button>
           </Field>
           <FieldSeparator>Or</FieldSeparator>
           <Field className="grid gap-4 sm:grid">
 
-            <Button variant="outline" type="button">
+            <Button
+             className="bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-400 text-neutral-800 transition-all duration-200 ease-in-out active:scale-95"
+             variant="outline" type="button">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path
                   d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
