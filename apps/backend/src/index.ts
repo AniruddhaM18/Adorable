19f3e5498b10d673d5a2056e893d4aebcaf9be83
Aniruddha <aniruddhamaradwar9@gmail.com>
2026-01-04 23:26:17 +0530

    backend-connectivity

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,22 +1,30 @@
 import express from "express";
+import cors from "cors";
 import projectRouter from "./routes/projectRouter.js";
 import authRouter from "./routes/authRouter.js";
 import cookieParser from "cookie-parser";
+import { tr } from "zod/locales";
 
 
 const app = express();
+
+//cors before routes
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true, //allow cookies,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+}))
+
 app.use(cookieParser());     
 app.use(express.json());
+
 
 app.use("/project", projectRouter);
 
 app.use("/auth", authRouter);
 
-app.post("/amrit", async(req, res) => {
-
-});
-
-app.listen(3000, () => {
-    console.log("Server running @:3000")
+app.listen(3001, () => {
+    console.log("Server running @:3001")
 })
 
