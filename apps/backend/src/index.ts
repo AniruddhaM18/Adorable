91672f5abcace9c997bdd315cd8e2d6004e998c0
Aniruddha <aniruddhamaradwar9@gmail.com>
2026-02-01 18:55:06 +0530

    update

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,12 +4,12 @@ import projectRouter from "./routes/projectRouter.js";
 import authRouter from "./routes/authRouter.js";
 import cookieParser from "cookie-parser";
 import { tr } from "zod/locales";
+import chatRouter from "./routes/chatRoutes.js";
 
 
 const app = express();
 
 //cors before routes
-
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true, //allow cookies,
@@ -23,6 +23,8 @@ app.use(express.json());
 app.use("/project", projectRouter);
 
 app.use("/auth", authRouter);
+
+app.use("/api", chatRouter);
 
 
 app.listen(3001, () => {
